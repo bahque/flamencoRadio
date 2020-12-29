595a72f94a358683d78534aead9882666b46f0d1
bahque <bahque@gmail.com>
2020-12-29 12:05:10 +0100

    smetto, che non son capace...

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ use std::io::copy;
 fn foo(url: String) -> Result<(), Box<dyn std::error::Error>> {
     let mut resp = reqwest::blocking::get(&url)?;
     //let bytes = resp.bytes()?;
+    // https://cdnlive.shooowit.net/rtvalive/smil:channel7.smil/chunklist_b250000.m3u8
+    // https://cdnlive.shooowit.net/rtvalive/smil:channel7.smil/chunklist_b250000.m3u8
+    // https://cdnlive.shooowit.net/rtvalive/smil:channel7.smil/media_b250000_295802.aac
+    // https://cdnlive.shooowit.net/rtvalive/smil:channel7.smil/media_b250000_299944.aac
     let mut dest = File::create("target/dest")?;
     //resp.copy_to(&mut std::io::stdout())?;
     resp.copy_to(&mut dest)?;
@@ -18,4 +22,4 @@ fn main() {
     println!("Hello, world!");
 }
 
-//  EditCount 42
+//  EditCount 48
